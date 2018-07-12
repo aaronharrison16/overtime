f4a2a91e8d2ec623fbf1164ce6017c9dcb6ce628
Aaron Harrison <aaronharrison16@gmail.com>
2018-07-11 21:34:53 -0600

    hid posts from the users that did not create them

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -20,9 +20,20 @@ describe 'navigate' do
 
     it 'has a list of posts' do
       post1 = FactoryGirl.build_stubbed(:post)
-      post1 = FactoryGirl.build_stubbed(:second_post)
+      post2 = FactoryGirl.build_stubbed(:second_post)
       visit posts_path
       expect(page).to have_content(/Rationale|content/)
+    end
+
+    it 'has a scope so only post creators can see thier posts' do
+      post1 = Post.create(date: Date.today, rationale: 'asdfrasdf', user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: 'asdfrasdf', user_id: @user.id)
+
+      other_user = User.create(first_name: 'Other', last_name: 'User', email: 'other@user.com', password: 'password',  password_confirmation: 'password')
+      post_from_other_user = Post.create(date: Date.today, rationale: 'This post should not be seen', user_id: other_user.id)
+      visit posts_path
+
+      expect(page).to_not have_content(/This post should not be seen/)
     end
   end
 
@@ -32,12 +43,13 @@ describe 'navigate' do
 
       click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
-    end
+    end 
   end
 
   describe 'delete' do 
     it 'can be deleted' do
       @post = FactoryGirl.create(:post)
+      @post.update(user_id: @user.id)
       visit posts_path
 
       click_link("delete_post_#{@post.id}_from_index")
