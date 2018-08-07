094596a5af5aa8faf7f17eb4697ca9250bc80dc9
Aaron Harrison <aaronharrison16@gmail.com>
2018-08-07 12:11:13 -0600

    integrated basic maneger notification email.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,7 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@aaronjharrison.com'}
